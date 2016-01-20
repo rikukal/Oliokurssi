c9c66c3735bc9b60660a23ba4f403ec48a73f37d
rikukal <h8851@student.jamk.fi>
2016-01-20 11:12:10 +0200

    Kommenttipäivitys T6

--- Demo01/T6/Program.cs
+++ Demo01/T6/Program.cs
@@ -10,14 +10,25 @@ namespace T6
     {
         static void Main(string[] args)
         {
-    /* Auton kulutus on 7.02 litraa 100 kilometrin matkalla ja bensan hinta on 1.595 Euroa. 
-    Tee ohjelma, joka tulostaa ajetulla matkalla (kysytään käyttäjältä) kuluvan bensan määrän sekä bensaan menevän rahan määrän.
-    Esimerkkitoiminta:
-    Anna matka > 250 [Enter]
-    Bensaa kuluu 17,55 litraa, kustannus 27,99 euroa
-    */
+            /* Auton kulutus on 7.02 litraa 100 kilometrin matkalla ja bensan hinta on 1.595 Euroa. 
+            Tee ohjelma, joka tulostaa ajetulla matkalla (kysytään käyttäjältä) kuluvan bensan määrän sekä bensaan menevän rahan määrän.
+            Esimerkkitoiminta:
+            Anna matka > 250 [Enter]
+            Bensaa kuluu 17,55 litraa, kustannus 27,99 eurolhlha
+            */
+            Console.WriteLine("Kuinka monta kilometriä ajoit?");
+
+            string line = Console.ReadLine();
+
+            int seconds = int.Parse(line);
+
+            int hour = seconds / 3600;                      //    Tähän matka                 
+            int min = seconds / 60 - hour * 60;             //  Tähän bensan kulutus
+            int second = seconds % 60;                      //      Tässä tuhlattu € -bensasta 
 
 
+
+            Console.WriteLine("Ajoit:{0}km, kulutit {1} litraa bensaa ja rahaa bensaan kului {2} euroa", matka, bensa, euro);
         }
     }
 }
