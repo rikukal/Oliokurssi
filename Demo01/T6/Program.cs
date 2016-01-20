d88133d1ae8ba58ca1cf05c55b8fcd4f45d7e573
rikukal <h8851@student.jamk.fi>
2016-01-20 10:58:49 +0200

    Päivitys T6

--- Demo01/T6/Program.cs
+++ Demo01/T6/Program.cs
@@ -10,6 +10,14 @@ namespace T6
     {
         static void Main(string[] args)
         {
+    /* Auton kulutus on 7.02 litraa 100 kilometrin matkalla ja bensan hinta on 1.595 Euroa. 
+    Tee ohjelma, joka tulostaa ajetulla matkalla (kysytään käyttäjältä) kuluvan bensan määrän sekä bensaan menevän rahan määrän.
+    Esimerkkitoiminta:
+    Anna matka > 250 [Enter]
+    Bensaa kuluu 17,55 litraa, kustannus 27,99 euroa
+    */
+
+
         }
     }
 }
